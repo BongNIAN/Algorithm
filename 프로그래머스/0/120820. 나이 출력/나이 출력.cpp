f1fcfc2fdfe4794793b0d7b57eee85eec861b8d0
KKuBong <165163630+KKuBong@users.noreply.github.com>
2024-12-26 10:22:18 +0900

    [level 0] Title: 나이 출력, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.cpp
@@ -0,0 +1,17 @@
+#include <string>
+// #include <vector>
+using namespace std;
+
+int solution(int age) {
+    int year = 2022;
+   
+    // cin>>age;
+    // if(age <0||age>120){return -1;}
+    // else { } 
+        
+ int answer = year - age +1;
+    
+    
+    return answer;
+    
+}
