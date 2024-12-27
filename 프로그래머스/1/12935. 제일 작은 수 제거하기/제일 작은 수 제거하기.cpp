77061e850a7c3d3a1b67d3cc3aca96d09d9101c2
BongNIAN <165163630+BongNIAN@users.noreply.github.com>
2024-12-27 22:48:54 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 10.73 ms, Memory: 13.3 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.cpp
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.cpp
@@ -1,11 +1,55 @@
+// #include <string>
+// #include <vector>
+// #include <algorithm>
+// using namespace std;
+
+// vector<int> solution(vector<int> arr) {
+    
+//     int min=0;
+//     if(arr.size()<=1) {
+//         if(arr.size()==0){arr.push_back(-1);
+//                         }
+//         for(int i=0;i<arr.size();i++){
+//             arr.pop_back();
+//         }
+//         arr.push_back(-1);
+        
+//         return arr;
+//     }
+    
+//     //한바퀴 n 그다음 n 2n정도면 괜찮은건가? 
+//     for(int i=0;i<arr.size();i++){
+//         if(i==0){min = arr[i];}
+//         if(i!=0){
+//             if(min > arr[i]){
+                
+//                 min = arr[i];
+//             }    
+//         }
+//     }
+    
+    
+//     for(int i=0;i<arr.size();i++){
+        
+//       arr.erase(remove(arr.begin(), arr.end(),min),arr.end());
+        
+//     }
+    
+    
+    
+    
+    
+//     return arr;
+// }
+
 #include <string>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
 vector<int> solution(vector<int> arr) {
     
     int min=0;
+    int min_locate = 0;
     if(arr.size()<=1) {
         if(arr.size()==0){arr.push_back(-1);
                         }
@@ -19,21 +63,22 @@ vector<int> solution(vector<int> arr) {
     
     //한바퀴 n 그다음 n 2n정도면 괜찮은건가? 
     for(int i=0;i<arr.size();i++){
-        if(i==0){min = arr[i];}
+        if(i==0){min = arr[i];
+                min_locate = i;
+                }
         if(i!=0){
             if(min > arr[i]){
-                
+                min_locate = i;
                 min = arr[i];
             }    
         }
     }
     
     
-    for(int i=0;i<arr.size();i++){
+
+      arr.erase(arr.begin()+min_locate);
         
-      arr.erase(remove(arr.begin(), arr.end(),min),arr.end());
-        
-    }
+
     
     
     
