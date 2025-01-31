9c118de3d5c81bef11bac5adead5df4426b26c3e
BongNIAN <165163630+BongNIAN@users.noreply.github.com>
2025-01-31 09:34:38 +0900

    [level 1] Title: 소수 만들기, Time: 17.10 ms, Memory: 4.23 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+bool IsPrimeNum(int a)
+{
+    int cnt=2;
+   while(cnt!=a)
+   {
+       if(a%cnt==0) return false;
+       cnt++;
+   }
+    cout<<"cnt is : "<<a<<endl;
+    return true;
+}
+
+int solution(vector<int> nums) {
+    int answer = 0;
+
+    for(int i=0;i<nums.size();i++)
+    {
+        for(int j=i+1;j<nums.size();j++)
+        {
+            for(int k=j+1;k<nums.size();k++)
+            {
+                if(IsPrimeNum(nums[i]+nums[j]+nums[k]))answer++;
+            }
+        }
+    }
+
+    return answer;
+}
